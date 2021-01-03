02dfbb97783905598d14854cc99de6376af4c762
Manoj Shukla <mshukla@allsorter.com>
2021-01-03 17:15:58 +0000

    2nd commit

--- index.py
+++ index.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from typing import Optional
 from enum import Enum
 
 app = FastAPI()
+
+fake_item_db = [
+    {"item_name":"foo"},
+    {"item_name":"Bar"}, 
+    {"item_name":"Baz"},
+    {"item_name":"Bar_foo"}, 
+    {"item_name":"Baz_foo"},
+    {"item_name":"Bar_g"}, 
+    {"item_name":"Baz_ghww"},
+    {"item_name":"Baz_ghss"},
+    {"item_name":"Baz_ghs"},
+    {"item_name":"Baz_ghf"}
+]
 
 @app.get("/")
 async def root():
@@ -28,3 +42,18 @@ async def root(model_name:ModelName):
         return {"model_name": model_name,"message":"LeCNN all the images"}
     
     return {"model_name": model_name,"message":"Have someresiduals"}
+
+@app.get("/items/")
+async def root(skip:int = 0, limit:int =10):
+    return fake_item_db[skip: limit]
+
+
+#Optional Paremeter
+
+@app.get('/items/{item_id}')
+async def root(item_id:str, q: Optional[str] = None, short: bool= False):
+    if q:
+        return {"item_id":item_id, "q":q}
+    if not short:
+        return {"item_id":item_id, "short":short}
+    return {"item_id":item_id}
